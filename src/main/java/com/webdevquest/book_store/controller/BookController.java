a3fa528f24fa9800f688afd59a4390cd1e3a9764
jr-frazier <jrfrazier.dev@gmail.com>
2026-03-02 13:35:38 -0500

    added basic endpoint and swagger config

--- src/main/java/com/webdevquest/book_store/controller/BookController.java
+++ src/main/java/com/webdevquest/book_store/controller/BookController.java
@@ -1,14 +1,32 @@
 package com.webdevquest.book_store.controller;
 
+import com.webdevquest.book_store.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
 
-    @GetMapping("/api/v1/hello")
-    public String firstAPI() {
-        return "Hello World!";
+    private final List<Book> books = new ArrayList<>();
+
+    private void initializeBooks() {
+        books.addAll(List.of(
+                new Book("The Hobbit", "JRR Tolken", "Fantasy"),
+                new Book("The Lord of the Rings", "JRR Tolken", "Fantasy"),
+                new Book("The Hunger Games", "John Doe", "Fantasy")
+        ));
+    }
+
+    public BookController() {
+        initializeBooks();
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        return books;
     }
 
 }
